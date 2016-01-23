267a855d5c554b4d7acbdfb38d3a0d67a4d60047
dhust <rafaelmoura.dev@gmail.com>
2016-01-22 23:11:44 -0200

    modifying seconds

--- src/timewrp.h
+++ src/timewrp.h
@@ -4,12 +4,22 @@
 
 struct Seconds
 {
-	Seconds() = default;
+	constexpr Seconds() noexcept : 
+		m_seconds(0)
+	{
+	
+	}
+
 	constexpr Seconds::Seconds(float seconds)  noexcept :
-	m_seconds(cexpr_div((float)CLOCKS_PER_SEC, seconds))
+		m_seconds(cexpr_div((float)CLOCKS_PER_SEC, seconds))
 	{
 	}
-	operator std::clock_t() {
+	
+	Seconds& operator=(const float seconds) noexcept {
+		m_seconds = cexpr_div((float)CLOCKS_PER_SEC, seconds);
+	}
+
+	constexpr operator std::clock_t() noexcept {
 		return static_cast<clock_t>(m_seconds);
 	}
 private:
