796aaa2cef94d86607f9ff831d24e683d89a0a45
dhust <rafaelmoura.dev@gmail.com>
2016-09-08 00:24:35 -0300

    init up
    getting some draw tests

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,16 +1,64 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+constexpr const int WIN_WIDTH = 512;
+constexpr const int WIN_HEIGHT = 256;
+
+constexpr const float BALL_RADIUS = 10.5;
+constexpr const float BALL_VELOCITY = 7.5;
+
+struct Position : sf::Vector2f {
+	using sf::Vector2f::Vector2;
+};
+struct Velocity : sf::Vector2f {
+	using sf::Vector2f::Vector2;
+};
 
 
+struct GameObjects {
+	sf::CircleShape ball {BALL_RADIUS};
+};
 
-int main() {
+struct Positions {
+	Position ball { WIN_WIDTH / 2, WIN_HEIGHT / 2 };
+};
 
-	#if (defined(__PONGCPP_DEBUG__) && defined(DEBUG))
-	std::cout << "HELLO DEBUG!\n"; 
-	#elif (defined(__PONGCPP_RELEASE__) && defined(NDEBUG))
-	std::cout << "HELLO RELEASE!\n";
-	#else
-	std::cout << "HELLO UNKNOWN!\n";
-	#endif
+struct Velocities {
+	Velocity ball { BALL_VELOCITY, BALL_VELOCITY };
+};
+
+
+static void update_objects(GameObjects* objects, Positions* positions, Velocities* velocities);
+
+
+int main()
+{
+	GameObjects objects;
+	Positions positions;
+	Velocities velocities;
+	
+	sf::RenderWindow window({WIN_WIDTH, WIN_HEIGHT}, "PongCpp");
+	sf::Event event;
+	
+	window.setFramerateLimit(60);
+	
+	while (window.isOpen()) {
+		
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+				break;
+			}
+		}
+		
+		update_objects(&objects, &positions, &velocities);
+		
+		window.clear(sf::Color::Red);
+		
+		window.draw(objects.ball);
+		
+		window.display();
+	}
 
 	return 0;
 }
@@ -19,6 +67,35 @@ int main() {
 
 
 
+
+
+
+
+
+
+static void update_objects(GameObjects* const objects,
+                           Positions* const positions,
+                           Velocities* const velocities)
+{
+	{
+		auto& ballpos = positions->ball;
+		auto& ballvel = velocities->ball;
+		
+		if (ballpos.x < 0)
+			ballvel.x = -ballvel.x;
+		else if (ballpos.x > WIN_WIDTH)
+			ballvel.x = -ballvel.x;
+		if (ballpos.y < 0)
+			ballvel.y = -ballvel.y;
+		else if (ballpos.y > WIN_HEIGHT)
+			ballvel.y = -ballvel.y;
+		
+		ballpos += ballvel;
+	}
+
+
+	objects->ball.setPosition(positions->ball);
+}
 
 
 
